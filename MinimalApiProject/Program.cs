b3f4b4a09b11b0bb6f3bb17ae4a28b4863cac87c
𝒟𝓇𝒾 <162618323+drielDev@users.noreply.github.com>
2024-06-28 10:17:36 -0300

    react

--- MinimalApiProject/Program.cs
+++ MinimalApiProject/Program.cs
@@ -1,6 +1,101 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/", () => "Prova A1");
+
+//ENDPOINTS DE CATEGORIA
+//GET: http://localhost:5273/categoria/listar
+app.MapGet("/categoria/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Categorias.Any())
+    {
+        return Results.Ok(ctx.Categorias.ToList());
+    }
+    return Results.NotFound("Nenhuma categoria encontrada");
+});
+
+//POST: http://localhost:5273/categoria/cadastrar
+app.MapPost("/categoria/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Categoria categoria) =>
+{
+    ctx.Categorias.Add(categoria);
+    ctx.SaveChanges();
+    return Results.Created("", categoria);
+});
+
+//ENDPOINTS DE TAREFA
+//GET: http://localhost:5273/tarefas/listar
+app.MapGet("/tarefas/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//POST: http://localhost:5273/tarefas/cadastrar
+app.MapPost("/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Tarefa tarefa) =>
+{
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria não encontrada");
+    }
+    tarefa.Categoria = categoria;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
+
+//PUT: http://localhost:5273/tarefas/alterar/{id}
+app.MapPut("/tarefas/alterar/{id}", (
+    [FromRoute] string id,
+    [FromBody] Tarefa tarefaAlterado,
+    [FromServices] AppDataContext ctx) =>
+{
+    //Implementar a alteração do status da tarefa
+    Tarefa? tarefa = ctx.Tarefas.Find(id);
+    if (tarefa is null)
+    {
+        return Results.NotFound("Tarefa não encontrada!");
+    }
+    tarefa.Status = tarefaAlterado.Status;
+    ctx.Tarefas.Update(tarefa);
+    ctx.SaveChanges();
+    return Results.Ok("Tarefa alterada com sucesso!");
+});
+
+//GET: http://localhost:5273/tarefas/naoconcluidas
+app.MapGet("/tarefas/naoconcluidas", ([FromServices] AppDataContext ctx) =>
+{
+    var tarefasNaoConcluidas = ctx.Tarefas.Where(t => t.Status == "Não iniciada" || t.Status == "Em andamento" ).ToList();
+
+    if (tarefasNaoConcluidas.Any())
+    {
+        return Results.Ok(tarefasNaoConcluidas);
+    }
+
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//GET: http://localhost:5206/tarefas/concluidas
+app.MapGet("/tarefas/concluidas", ([FromServices] AppDataContext ctx) =>
+{
+    var tarefasConcluidas = ctx.Tarefas.Where(t => t.Status == "Concluída").ToList();
+
+    if (tarefasConcluidas.Any())
+    {
+        return Results.Ok(tarefasConcluidas);
+    }
+
+    return Results.NotFound("Nenhuma tarefa concluída encontrada");
+});
 
 app.Run();
